24180f26bf65ac25975c53d3b75c4aff0151e4bb
James Ellis <james.e.ellis@ntlworld.com>
2023-05-16 07:19:08 +0100

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, HostListener} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'LandingPage2';
+
+  @HostListener('window:scroll', [])
+  public onWindowScroll(): void {
+    const scrollPosition: number = window.scrollY;
+    const headerElements: NodeListOf<HTMLElement> = document.querySelectorAll('.app-headerTextNavOption, .app-headerLogo');
+    const headlineElement: HTMLElement = document.querySelector('.app-headlineContainer')!;
+    const heroImageContainer: HTMLElement = document.querySelector('.app-heroImageContainer')!;
+    headerElements.forEach((element: HTMLElement) => {
+      element.style.transform = `translateY(${-scrollPosition}px)`;
+    });
+    headlineElement.style.transform = `translateY(${-scrollPosition / 4}px)`
+    heroImageContainer.style.clipPath = `inset(${5 - scrollPosition / 125}vw)`
+  }
 }
