b92f90874978a4244b9d82bc93b961191f7bb437
James Ellis <james.e.ellis@ntlworld.com>
2023-05-21 09:29:49 +0100

    added about section

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,25 @@
-import {Component, HostListener} from '@angular/core';
+import {Component, HostListener, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'LandingPage2';
+  reveals: HTMLElement[] = [];
+
+  ngOnInit() {
+    this.reveals = Array.from(document.querySelectorAll('.reveal')) as HTMLElement[];
+  }
 
   @HostListener('window:scroll', [])
   public onWindowScroll(): void {
+    this.scrollingAnimations();
+    this.reveal();
+  }
+
+  scrollingAnimations() {
     const scrollPosition: number = window.scrollY;
     const headerElements: NodeListOf<HTMLElement> = document.querySelectorAll('.app-headerTextNavOption, .app-headerLogo');
     const headlineElement: HTMLElement = document.querySelector('.app-headlineContainer')!;
@@ -19,5 +29,19 @@ export class AppComponent {
     });
     headlineElement.style.transform = `translateY(${-scrollPosition / 4}px)`
     heroImageContainer.style.clipPath = `inset(${5 - scrollPosition / 125}vw)`
+  }
+
+  reveal() {
+    const windowHeight = window.innerHeight;
+    const elementVisible = 100;
+
+    this.reveals.forEach(reveal => {
+      const elementTop = reveal.getBoundingClientRect().top;
+
+      if (elementTop < windowHeight - elementVisible) {
+        console.log(reveal.classList);
+        reveal.classList.add('active');
+      }
+    });
   }
 }
